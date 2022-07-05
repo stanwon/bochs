d4cba1b32dfd34fa81620f89952be0e8e36ce36d
stan <stan_wong@foxmail.com>
2022-07-05 20:37:53 +0800

    add kernel

--- shell/up.sh
+++ shell/up.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 dd if=../out/mbr.bin of=../src/other/hd60M.img bs=512 count=1 conv=notrunc
 dd if=../out/loader.bin of=../src/other/hd60M.img bs=512 count=4 seek=2 conv=notrunc
+dd if=../out/kernel.bin of=../src/other/hd60M.img bs=512 count=200 seek=9 conv=notrunc
 bochs -f ../src/other/bochsrc.disk
