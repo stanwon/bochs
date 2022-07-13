6c9fed9f3bc518c772550b887ac3ab09bd491c9c
stan <stan_wong@foxmail.com>
2022-07-13 20:24:28 +0800

    add put_char function

--- ch5/readme.md
+++ ch5/readme.md
@@ -1,3 +1,5 @@
 1. 检测最大内存容量
 2. 开启分页
 3. 内核(有修改，与教材中不同)
+	todo 
+		尝试删除节的方法：strip --remove-section=.note.gnu.property kernel.bin
---
+++ ch6/1/code/kernel/main.c
@@ -0,0 +1,20 @@
+#include "print.h"
+int main(void) {
+  put_char('h');
+  put_char('e');
+  put_char('l');
+  put_char('l');
+  put_char('o');
+  put_char('\n');
+  put_char('w');
+  put_char('o');
+  put_char('r');
+  put_char('l');
+  put_char('d');
+  put_char(',');
+  put_char('\b');
+  put_char('!');
+  while (1)
+    ;
+  return 0;
+}
