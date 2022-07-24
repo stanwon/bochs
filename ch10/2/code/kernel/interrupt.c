838b623aedc826d6ed523961f2facf4918a5898e
Stan <stan_wong@foxmail.com>
2022-07-24 18:45:56 +0800

    add some code

--- ch10/2/code/kernel/interrupt.c
+++ ch10/2/code/kernel/interrupt.c
@@ -46,8 +46,8 @@ static void pic_init(void) {
   outb(PIC_M_DATA, 0xfe);
   outb(PIC_S_DATA, 0xff);
 
-  //outb(PIC_M_DATA, 0xfd);
-  //outb(PIC_S_DATA, 0xff);
+  outb(PIC_M_DATA, 0xfd);
+  outb(PIC_S_DATA, 0xff);
 
   put_str("pic_init done\n");
 }
--- ch10/2/code/kernel/main.c
+++ ch10/2/code/kernel/main.c
@@ -13,8 +13,8 @@ int main(void) {
   put_str("This is kernel\n");
   init_all();
 
-  thread_start("k_thread_a", 31, k_thread_a, "argA ");
-  thread_start("k_thread_b", 8, k_thread_b, "argB ");
+//  thread_start("k_thread_a", 31, k_thread_a, "argA ");
+//  thread_start("k_thread_b", 8, k_thread_b, "argB ");
 
   intr_enable();
   while (1) {
@@ -23,7 +23,7 @@ int main(void) {
     put_str("main ");
     intr_enable();
 #endif
-    console_put_str("test ");
+//    console_put_str("test ");
   }
   return 0;
 }
